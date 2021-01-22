1143b136ecef1780da31b04682fda415810ec4af
Mohamed Achraf Abdelbari <med.achraf.abdelbari@gmail.com>
2021-01-22 18:57:13 +0100

    updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,12 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
   production: true,
   configUrl: 'assets/config/globalSettings.json',
-  defaultPingDelayTime: 30,
+  defaultPingDelayTime: 180,
   sendSms: true,
   sendEmail: true,
   destinationEmail: 'med.achraf.abdelbari@gmail.com',
@@ -14,14 +19,85 @@ export const environment = {
       Client_Ref: '1118305343072',
       Billing_Ref: 'iceland',
       Connection: '2',
-      Originator: 'Monitoring platform',
+      Originator: 'Monitoring-platform',
       OType: '1',
-      DestinationEx: '7451279340',
+      DestinationEx: '07541641955',
       SMS_Type: '0',
-      Body: '',
-      Reply_Type: ''
+      Reply_Type: '0'
     },
     port: 80
   },
-  emailServiceConfig: {}
+  emailServiceConfig: {},
+  services: [
+    {
+      'name': 'Multi LLC',
+      'url': 'https://goprime.monika.com/prime',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Multi PTY',
+      'url': 'https://ozmulti.monika.com/prime',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Multi UK',
+      'url': 'https://connect.monika.com/prime',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Capital Hospitality',
+      'url': 'https://caphosp.monika.com/monika',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Compass24',
+      'url': 'https://compass24.monika.com/monika',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Facebook Sydney',
+      'url': 'https://fb.monika.com/monika',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Hilton Garden Inn Tabuk',
+      'url': 'https://ht.monika.com/monika',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'IKEA',
+      'url': 'https://ikea.monika.com/monika',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Iceland',
+      'url': 'https://cloud.monika.com/iceland',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Meraas',
+      'url': 'https://meraas.monika.com/monika',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'SECAmb',
+      'url': 'https://secamb.monika.com/monika',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Southport and Ormskirk',
+      'url': 'https://southorms.monika.com/monika',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'The Range',
+      'url': 'https://web.monika.com/therange',
+      'activeStatusCode': 200
+    },
+    {
+      'name': 'Test Api',
+      'url': 'https://dummyapi.io/explorer',
+      'activeStatusCode': 200
+    }
+  ]
 };
